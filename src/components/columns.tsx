476cb178ec7ba1d68a6727a0c1bdee1ea6fd2fd2
Ankita Mohanty <ashishmohaty10@gmail.com>
2024-05-21 03:38:01 +0530

    implemented add remove drag feature

--- src/components/columns.tsx
+++ src/components/columns.tsx
@@ -1,11 +1,17 @@
 import Column from "./column";
+import NewTodoDialog from "./newtododialog";
+
+import { Dialog } from "./ui/dialog";
 
 export default function Columns() {
   return (
-    <section className="mt-10 flex gap-6 lg:gap-12">
-      <Column title="Todo" status="TODO" />
-      <Column title="In Progress" status="IN_PROGRESS" />
-      <Column title="Done" status="DONE" />
-    </section>
+    <div>
+      <NewTodoDialog />
+      <section className="mt-10 flex gap-6 lg:gap-12">
+        <Column title="Todo" status="TODO" />
+        <Column title="In Progress" status="IN_PROGRESS" />
+        <Column title="Done" status="DONE" />
+      </section>
+    </div>
   );
 }
