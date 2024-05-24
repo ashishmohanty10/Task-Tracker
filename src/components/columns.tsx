6038a4ff72b32f8c42a2ac369d1a7c1e5498f647
Ankita Mohanty <ashishmohaty10@gmail.com>
2024-05-24 23:48:05 +0530

    resposive done

--- src/components/columns.tsx
+++ src/components/columns.tsx
@@ -6,8 +6,10 @@ import { Dialog } from "./ui/dialog";
 export default function Columns() {
   return (
     <div>
-      <NewTodoDialog />
-      <section className="mt-10 flex gap-6 lg:gap-12">
+      <div className="mb-5">
+        <NewTodoDialog />
+      </div>
+      <section className="mt-0 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-20 ">
         <Column title="Todo" status="TODO" />
         <Column title="In Progress" status="IN_PROGRESS" />
         <Column title="Done" status="DONE" />
